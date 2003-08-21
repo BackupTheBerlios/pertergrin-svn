5bf09efc6ffd42ccabe1c304015f61368f0a3096
suamor <suamor@949e3a5a-044f-4f06-a0e8-827bf912e4ed>
2003-08-21 19:44:23 +0000

    main function

---
+++ trunk/pertergrin/editor/kernel/main.cc
@@ -0,0 +1,30 @@
+/* main.cc - Pertergrin Editor main function which creates the main window
+ *
+ * Copyright (C) 2001-2002 Pertergrin development team
+ *
+ * It is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License,  or (at your option) any later
+ * version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not contact one the AUTHORS.
+ */
+
+#include <gtkmm/main.h>
+#include "mainwindow.h"
+
+int main()
+
+{
+  Gtk::Main kit(argc,argv);
+  PTGMainWindow oMainWin;
+
+  kit.run(oMainWin);
+  return (0);
+}
