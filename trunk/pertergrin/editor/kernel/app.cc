0ca83840a1776cf16a1d1558109f7930aac6db2b
suamor <suamor@949e3a5a-044f-4f06-a0e8-827bf912e4ed>
2003-09-07 18:28:20 +0000

    Application kernel of Pertergrin: Connection between UI and underlying world management and the module export

---
+++ trunk/pertergrin/editor/kernel/app.cc
@@ -0,0 +1,62 @@
+/* app.cc - Pertergrin Editor class definition of the application management
+ *
+ * Copyright (C) 2001-2003 Pertergrin development team
+ *
+ * It is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License,  or (at your option) any later
+ * version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not contact one the AUTHORS.
+ */
+
+#include "app.h"
+
+// Empty default constructor
+PtgApp::PtgApp()
+{
+}
+
+// Creates a new world
+// Input parameters:
+// sWorldData  -  Definition of the world
+void PtgApp::createWorld(pworld_t sWorldData)
+{
+  auto_ptr<PtgWorld> poWorld(new PtgWorld(sWorldData.iXSize, sWorldData.iYSize, 
+                                          sWorldData.oName, sWorldData.iWidth, 
+                                          sWorldData.iHeight));
+
+  // Transfer Pointer
+  mpoWorld = poWorld;
+  mpoWorld->setDescription(sWorldData.oDescription);
+  //mpoWorld->setWorldType(WT_Pertergrin);
+  //mpoWorld->setRegionSize(sWorldData.iRegionXSize, sWorldData.iRegionYSize);
+  //mpoWorld->setStartDate(sWorldData.oStartDate);
+}
+
+// Gets the world
+// Return parameter:
+// Instance of the world
+auto_ptr<PtgWorld> &PtgApp::getWorld()
+{
+  return mpoWorld;
+}
+  
+// Sets/Changes the data of the world
+// Input parameters:
+// sWorldData  -  Definition of the world
+void PtgApp::setWorldData(pworld_t sWorldData)
+{
+  mpoWorld->setDescription(sWorldData.oDescription);
+  mpoWorld->setSize(sWorldData.iXSize, sWorldData.iYSize);
+  mpoWorld->setName(sWorldData.oName);
+  //mpoWorld->setWorldType(WT_Pertergrin);
+  //mpoWorld->setRegionSize(sWorldData.iRegionXSize, sWorldData.iRegionYSize);
+  //mpoWorld->setStartDate(sWorldData.oStartDate);
+}
---
+++ trunk/pertergrin/editor/kernel/app.h
@@ -0,0 +1,42 @@
+/* app.h - Pertergrin Editor class declaration of the application management
+ *
+ * Copyright (C) 2001-2003 Pertergrin development team
+ *
+ * It is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License,  or (at your option) any later
+ * version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not contact one the AUTHORS.
+ */
+
+#ifndef PTGAPP_H
+#define PTGAPP_H
+
+#include "ptgtypes.h"
+#include "world.h"
+#include <memory>
+
+class PtgApp
+{
+public:
+  PtgApp();
+
+  // World methods
+  void createWorld(pworld_t sWorldData);
+  void setWorldData(pworld_t sWorldData);
+  // Read only
+  auto_ptr<PtgWorld> &getWorld();
+
+private:
+  auto_ptr<PtgWorld> mpoWorld;
+
+};
+
+#endif // PTGAPP_H
