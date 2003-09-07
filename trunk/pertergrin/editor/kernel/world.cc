5728e0d677522bdc491088845e616ffb7a822f65
suamor <suamor@949e3a5a-044f-4f06-a0e8-827bf912e4ed>
2003-09-07 19:57:04 +0000

    More methods implemented

--- trunk/pertergrin/editor/kernel/world.cc
+++ trunk/pertergrin/editor/kernel/world.cc
@@ -33,26 +33,52 @@ PtgWorld::PtgWorld(int iXSize, int iYSize, Glib::ustring oName,
 
   msWorldData.iWidth = iTileWidth;
   msWorldData.iHeight = iTileHeight;
+
+  // Default selected region (nothing selected)
+  mapClearRegion();
 }
 
+// Clears the map
+// Do we really need this method ?
 void PtgWorld::mapClear(int iXPos, int iYPos, int iWidth, int iHeight)
 {
+  mapFill(iXPos, iYPos, iWidth, iHeight, 
+          msClearDefaultPart);
 }
 
+// Clears the map
+// Do we really need this method ?
 void PtgWorld::mapClear(int iWidth, int iHeight)
 {
+  mapFill(msWorldData.iRegionX, msWorldData.iRegionY, iWidth, iHeight, 
+          msClearDefaultPart);
 }
 
+// Clears the map
+// Do we really need this method ?
+void PtgWorld::mapClear()
+{
+  mapFill(msWorldData.iRegionX, msWorldData.iRegionY,
+          msWorldData.iRegionXSize, msWorldData.iRegionYSize, msClearDefaultPart);
+}
+
+// Sets a landscape part at a given position
+// Input parameters:
+// iXPos  - X-Position of the landscape map part
+// iYPos  - Y-Position of the landscape map part
+// sLPart  -  The landscape map part at position iXPos, iYPos
 void PtgWorld::mapSet(int iXPos, int iYPos, plpart_t sLPart)
 {
+  msWorldData.vecoRows = msWorldData.vecoColumns[iXPos];
+  msWorldData.vecoRows[iYPos] = sLPart;
 }
 
-// Get a world map part
+// Get a landscape map part from a given position
 // Input parameters:
-// iXPos  - X-Position of the world map part
-// iYPos  - Y-Position of the world map part
+// iXPos  - X-Position of the landscape map part
+// iYPos  - Y-Position of the landscape map part
 // Return parameter:
-// The world map part at position iXPos, iYPos
+// The landscape map part at position iXPos, iYPos
 plpart_t &PtgWorld::mapGet(int iXPos, int iYPos)
 {
   msWorldData.vecoRows = msWorldData.vecoColumns[iXPos];
@@ -64,12 +90,52 @@ void PtgWorld::mapMove(int iDestXPos, int iDestYPos, int iSourceXPos, int Source
 {
 }
 
+// Fills the specified region with a map piece
+// Warning: Will remove any edited map parts in the region!!!
+// Input parameters:
+// iX  -  X Position of the region
+// iY  -  Y Position of the region
+// iWidth  -  Width of the region
+// iHeigth  -  Height of the region
+// sLPart  -  Landscape map part to be set in the region
 void PtgWorld::mapFill(int iXPos, int iYPos, int iWidth, int iHeight, plpart_t sLPart)
 {
+  int iXC, iYC;
+  // Make sure the selection is not outside of the map
+  if( (iXPos + iWidth) > msWorldData.iXSize )
+    iWidth = msWorldData.iXSize - iXPos;
+  if( (iYPos + iHeight) > msWorldData.iYSize )
+    iWidth = msWorldData.iYSize - iYPos;
+  for( iXC=iXPos; iXC < iWidth; iXC++ )
+  {
+    msWorldData.vecoRows = msWorldData.vecoColumns[iXC];
+    for( iYC=iYPos; iYC < iHeight; iYC++ )
+    {
+      // Copy the landscape part data
+      msWorldData.vecoRows[iYC] = sLPart;
+    }
+  }
 }
 
+// Fills the specified (selected) region with a map piece
+// Warning: Will remove any edited map parts in the region!!!
+// Input parameters:
+// iWidth  -  Width of the region
+// iHeigth  -  Height of the region
+// sLPart  -  Landscape map part to be set in the region
 void PtgWorld::mapFill(int iWidth, int iHeight, plpart_t sLPart)
 {
+  mapFill(msWorldData.iRegionX, msWorldData.iRegionY, iWidth, iHeight, sLPart);
+}
+
+// Fills the selected region with a map piece
+// Warning: Will remove any edited map parts in the region!!!
+// Input parameters:
+// sLPart  -  Landscape map part to be set in the region
+void PtgWorld::mapFill(plpart_t sLPart)
+{
+  mapFill(msWorldData.iRegionX, msWorldData.iRegionY,
+          msWorldData.iRegionXSize, msWorldData.iRegionYSize, sLPart);
 }
 
 // Set the map default part to clear parts of the world map
@@ -78,6 +144,39 @@ void PtgWorld::mapFill(int iWidth, int iHeight, plpart_t sLPart)
 void PtgWorld::mapSetClearDefault(plpart_t sLPart)
 {
   msClearDefaultPart = sLPart;
+}
+
+// Select a region inside the world map
+// Input parameters:
+// iX  -  Selected region column
+// iY  -  Selected region row
+// iXSize  -  Number of Columns in the region
+// iYSize  -  Number of Rows in the region
+void PtgWorld::mapSelect(int iX, int iY, int iXSize, int iYSize)
+{
+  // Selected region has to be inside map
+  if( iX < 0 )
+    iX = 0;
+  if( iY < 0 )
+    iY = 0;
+  if( iXSize < 1 )
+    iXSize = 1;
+  if( iYSize < 1 )
+    iYSize = 1;
+
+  msWorldData.iRegionX = iX;
+  msWorldData.iRegionY = iY;
+  msWorldData.iRegionXSize = iXSize;
+  msWorldData.iRegionYSize = iYSize;
+}
+
+// Removes any selection inside the map
+void PtgWorld::mapClearRegion()
+{
+  msWorldData.iRegionX = 0;
+  msWorldData.iRegionY = 0;
+  msWorldData.iRegionXSize = 0;
+  msWorldData.iRegionYSize = 0;
 }
 
 // Set the size of the world
@@ -102,6 +201,15 @@ void PtgWorld::setSize(int iXSize, int iYSize)
 
   iXSize = msWorldData.iXSize;
   iYSize = msWorldData.iYSize;
+  // Reserve size so vectors do not reallocate all the time
+  // Attention: for real size maps > 1000 you may not use the complete
+  // size but only the size which fits in memory!
+  msWorldData.vecoColumns.reserve(iXSize);
+  for(int i=0; i<iXSize; i++)
+  {
+    msWorldData.vecoRows = msWorldData.vecoColumns[i];
+    msWorldData.vecoRows.reserve(iYSize);
+  }  
 }
 
 // Get the size of the world
