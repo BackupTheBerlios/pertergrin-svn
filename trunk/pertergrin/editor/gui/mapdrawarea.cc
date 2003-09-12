b35b74ace2320d6f8327f9417d9d0fb8885168c4
suamor <suamor@949e3a5a-044f-4f06-a0e8-827bf912e4ed>
2003-09-12 06:18:23 +0000

    Custom widget to draw the map

---
+++ trunk/pertergrin/editor/gui/mapdrawarea.cc
@@ -0,0 +1,142 @@
+/* mainwindow.cc - Pertergrin Editor class definition of the map draw area
+ *
+ * Copyright (C) 2001-2003 Pertergrin development team
+ *
+ * It is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License,  or (at your option) any later
+ * version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not contact one the AUTHORS.
+ */
+
+#include "mapdrawarea.h"
+
+// This should be removed ASAP
+// This class probably will never know anything about configuration
+#define PTG_PROTOTYPE 1
+
+/// Default-Konstruktor
+/// Input Parameters:
+/// opTileset  -  Default Pixbuf for drawing the map
+/// iXTileSize  -  size of a tile graphic
+/// iXTileSize  -  Y size of a tile graphic
+CWMapDrawArea::CWMapDrawArea(Glib::RefPtr<Gdk::Pixbuf> &opTileset, 
+                             int &iXTileSize, int &iYTileSize)
+  : mopDefaultTileset( opTileset ), miXTileSize( iXTileSize ), 
+    miYTileSize( iYTileSize )
+{
+  // First pixbuf
+  mopTilesets[0] = opTileset;
+  //moTempTile = (cwtpos_t)moDefaultTile;
+}
+
+/// Set the complete map
+/// Input Parameters:
+/// oTilesMap  -  Complete map to be drawn (will be copied!)
+/// iXSize  -  X size of the map
+/// iYSize  -  Y size of the map
+void CWMapDrawArea::setMap(cwtmap_t &oTilesMap, int iXSize, int iYSize)
+{
+#ifdef PTG_PROTOTYPE
+  // Currently prevent too large Maps
+  if( iXSize > 1000 )
+    iXSize = 1000;
+  if( iYSize > 1000 )
+    iYSize = 1000;
+#endif
+
+  // Prevent a too small world map
+  if( iXSize < 10 )
+    iXSize = 10;
+  if( iYSize < 10 )
+    iYSize = 10;
+
+  iMapXSize = iXSize;
+  iMapYSize = iYSize;
+  // Reserve size so vectors do not reallocate all the time
+  // Attention: for real size maps > 1000 you may not use the complete
+  // size but only the size which fits in memory!
+  moTilesMap.vecoColumns.reserve(iXSize);
+  for(int i=0; i<iXSize; i++)
+  {
+    moTilesMap.vecoRows = moTilesMap.vecoColumns[i];
+    moTilesMap.vecoRows.reserve(iYSize);
+  }  
+  moTilesMap = oTilesMap;
+}
+
+/// Changes a part map
+/// Input Parameters:
+/// iWidth  -  Width of the map part
+/// iHeight  -  Height of the map part
+/// iXPos  -  X Position in the map
+/// iYPos  -  Y Position in the map
+/// oTile  -  Tile used to draw the map part
+void CWMapDrawArea::changeMap(int iWidth, int iHeight, int iXPos, int iYPos,
+                              cwtpos_t &oTile)
+{
+  int iXC, iYC;
+  for( iXC=iXPos; iXC<iWidth; iXC++ )
+  {
+    moTilesMap.vecoRows = moTilesMap.vecoColumns[iXC];
+    for( iYC=iYPos; iYC < iHeight; iYC++ )
+      // Copy the tile data
+      moTilesMap.vecoRows[iYC] = oTile;
+  }
+}
+
+/// Sets the default tile 
+/// oTile  -  Tile to be set as default
+void CWMapDrawArea::setDefaultTile(cwtpos_t &oTile)
+{
+  moDefaultTile = oTile;
+}
+
+/// Sets the size of the tilesets
+/// iNum  -  Number of tilesets
+void CWMapDrawArea::setTilesetsSize(int iNum)
+{
+  mopTilesets.reserve(iNum);
+}
+
+/// Adds a tileset to the tilesets
+/// opTileset  -  New tileset
+/// iPos  -  Position in the tilesets
+void CWMapDrawArea::addTileset(Glib::RefPtr<Gdk::Pixbuf> &opTileset, int iPos)
+{
+  // Set tileset (size check ?)
+  mopTilesets[iPos] = opTileset;
+}
+
+void CWMapDrawArea::removeTileset(Glib::RefPtr<Gdk::Pixbuf> &opTileset, 
+                                  int iPos)
+{
+  //mopTilesets.erase(iPos);
+}
+  
+void CWMapDrawArea::clearTilesets()
+{
+  mopTilesets.clear();
+}
+
+bool CWMapDrawArea::on_map_area_expose (GdkEventExpose *event)
+{
+  return false;
+}
+
+bool CWMapDrawArea::on_map_area_value_changed()
+{
+  return false;
+}
+
+void CWMapDrawArea::adjust_scrollbars(void)
+{
+}
+
---
+++ trunk/pertergrin/editor/gui/mapdrawarea.h
@@ -0,0 +1,81 @@
+/* mapdrawarea.h - Pertergrin Editor class declaration of the map draw area
+ *
+ * Copyright (C) 2001-2003 Pertergrin development team
+ *
+ * It is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License,  or (at your option) any later
+ * version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not contact one the AUTHORS.
+ */
+
+#ifndef MAPDRAWAREA_H
+#define MAPDRAWAREA_H
+
+// includes
+#include <gtkmm.h>
+#include <glibmm.h>
+#include <vector>
+
+using namespace std;
+
+// Definition of a tile
+typedef struct cw_tile_pos
+{
+  Glib::ustring   oName;             /* Name of the graphic tile        */
+  int             iXPos;             /* X-Position inside tiles graphic */
+  int             iYPos;             /* Y-Position inside tiles graphic */
+} cwtpos_t;
+
+typedef struct cw_tile_map
+{
+  vector<cwtpos_t>           vecoRows;    /* Columns store all rows    */
+  vector< vector<cwtpos_t> > vecoColumns; /* Rows store the map itself */
+} cwtmap_t;
+
+/// Custom Widget Map (with Tiles) Draw Area
+/// To use it you need to have one or several tile sets.
+/// You need to have a map which defines where the tiles are places
+/// You don't need to define the whole map at once
+class CWMapDrawArea : public Gtk::DrawingArea
+{
+public:
+  CWMapDrawArea(Glib::RefPtr<Gdk::Pixbuf> &opTileset, 
+              int &iXTileSize, int &iYTileSize);
+  void setMap(cwtmap_t &oTilesMap, int iXSize=0, int iYSize=0);
+  void changeMap(int iWidth, int iHeight, int iXPos=0, int iYPos=0, 
+                 cwtpos_t &oTile=moTempTile);
+  void setDefaultTile(cwtpos_t &oTile);
+  void setTilesetsSize(int iNum);
+  void addTileset(Glib::RefPtr<Gdk::Pixbuf> &opTileset, int iPos);
+  void removeTileset(Glib::RefPtr<Gdk::Pixbuf> &opTileset, int iPos);
+  void clearTilesets();
+
+protected:
+  bool on_map_area_expose (GdkEventExpose *event);
+  bool on_map_area_value_changed();
+
+  void adjust_scrollbars (void);
+
+private:
+  Glib::RefPtr<Gdk::Pixbuf> &mopDefaultTileset;
+  Gtk::ScrolledWindow       moScrollPane;
+  Gtk::DrawingArea          moDrawingArea;
+  vector< Glib::RefPtr<Gdk::Pixbuf> > mopTilesets;
+  cwtmap_t moTilesMap;
+  int iMapXSize;
+  int iMapYSize;
+  int miXTileSize;
+  int miYTileSize;
+  cwtpos_t moDefaultTile;
+  static cwtpos_t &moTempTile;
+};
+
+#endif // MAPDRAWAREA_H
